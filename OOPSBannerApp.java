3fdcd15d520fcd925aa926ae8b5a238c81df183d
Ananya <aa7600@srmist.edu.in>
2026-03-04 17:05:02 +0530

    UC1: Added header comments

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,16 @@
+/**
+ * OOPS Banner App
+ * UC1: Print OOPS to console
+ * @author Ananya
+ * @version 1.0
+ */
+
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
+
         System.out.println("OOPS");
+
     }
+
 }
